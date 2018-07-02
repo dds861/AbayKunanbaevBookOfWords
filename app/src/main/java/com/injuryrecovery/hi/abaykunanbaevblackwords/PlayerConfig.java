f323a9cdcf444d86d037b625fe531d435eaf8607
Daniyar Nurgaliyev <dds861@users.noreply.github.com>
2018-07-02 03:16:42 +0200

    Update PlayerConfig.java

--- app/src/main/java/com/injuryrecovery/hi/abaykunanbaevblackwords/PlayerConfig.java
+++ app/src/main/java/com/injuryrecovery/hi/abaykunanbaevblackwords/PlayerConfig.java
@@ -3,5 +3,5 @@ package com.injuryrecovery.hi.abaykunanbaevblackwords;
 public class PlayerConfig {
     public PlayerConfig() {
     }
-    public static final String API_KEY = "AIzaSyCp2AXn4ULVpORtAHvweXu20PjkqTUC1iQ";
+    public static final String API_KEY = "";
 }
