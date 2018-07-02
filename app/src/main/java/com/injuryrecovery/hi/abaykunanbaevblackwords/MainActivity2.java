2b759a340ab4f1f34a6b732f5485c2926bf96776
dds861 <dds861@gmail.com>
2018-07-02 03:08:17 +0200

    added youtube audio to kazakh language

--- app/src/main/java/com/injuryrecovery/hi/abaykunanbaevblackwords/MainActivity2.java
+++ app/src/main/java/com/injuryrecovery/hi/abaykunanbaevblackwords/MainActivity2.java
@@ -2,21 +2,26 @@ package com.injuryrecovery.hi.abaykunanbaevblackwords;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import java.util.List;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends YouTubeBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
 
         //реклама
         AdView mAdView = findViewById(R.id.adView);
@@ -26,7 +31,7 @@ public class MainActivity2 extends AppCompatActivity {
         Intent mIntent = getIntent();
         String language = mIntent.getStringExtra("language");
         Log.i("autolog", "language: " + language);
-        int position = mIntent.getIntExtra("position", 0);
+        final int position = mIntent.getIntExtra("position", 0);
 
         DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
         databaseAccess.open();
@@ -39,5 +44,36 @@ public class MainActivity2 extends AppCompatActivity {
         s1 = s1.replaceAll("\\\\n", "\n");
         s1 = s1.replaceAll("\\\\t", "\t");
         mTvText.setText(s1);
+
+        setYoutubeVideo(position, language);
+
+
+    }
+
+    private void setYoutubeVideo(final int position, String language) {
+        final YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player);
+
+        if(language.equals(getResources().getString(R.string.kazakh))){youTubePlayerView.setVisibility(View.VISIBLE);}
+
+        final String[] mYoutubeLinks = getResources().getStringArray(R.array.youtube_links);
+
+        final YouTubePlayer.OnInitializedListener onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo(mYoutubeLinks[position]);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        youTubePlayerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                youTubePlayerView.initialize(PlayerConfig.API_KEY,onInitializedListener);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/injuryrecovery/hi/abaykunanbaevblackwords/PlayerConfig.java
@@ -0,0 +1,7 @@
+package com.injuryrecovery.hi.abaykunanbaevblackwords;
+
+public class PlayerConfig {
+    public PlayerConfig() {
+    }
+    public static final String API_KEY = "AIzaSyCp2AXn4ULVpORtAHvweXu20PjkqTUC1iQ";
+}
