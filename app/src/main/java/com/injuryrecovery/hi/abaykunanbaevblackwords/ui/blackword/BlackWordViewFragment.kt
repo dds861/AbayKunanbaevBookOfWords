eeb092f491efe96ae05129a00c22eec27f7e5aa7
Daniyar <dds861@gmail.com>
2020-06-28 23:27:42 +0200

    without youtube player

--- app/src/main/java/com/injuryrecovery/hi/abaykunanbaevblackwords/ui/blackword/BlackWordViewFragment.kt
+++ app/src/main/java/com/injuryrecovery/hi/abaykunanbaevblackwords/ui/blackword/BlackWordViewFragment.kt
@@ -1,12 +1,7 @@
 package com.injuryrecovery.hi.abaykunanbaevblackwords.ui.blackword
 
-import android.util.Log
-import android.widget.Toast
+import android.view.View
 import com.carmabs.ema.core.state.EmaExtraData
-import com.google.android.youtube.player.YouTubeInitializationResult
-import com.google.android.youtube.player.YouTubePlayer
-import com.google.android.youtube.player.YouTubePlayerFragment
-import com.google.android.youtube.player.YouTubePlayerSupportFragment
 import com.injuryrecovery.hi.abaykunanbaevblackwords.R
 import com.injuryrecovery.hi.abaykunanbaevblackwords.base.BaseToolbarsFragment
 import com.injuryrecovery.hi.abaykunanbaevblackwords.ui.checklist.LanguageName
@@ -47,11 +42,10 @@ class BlackWordViewFragment
     }
 
     override fun onNormal(data: BlackWordState) {
-        if (data.languageName == LanguageName.KAZAKH) {
-            setYoutubeVideo(data.position)
-            showYoutubePlayer()
-        } else {
-            hideYoutubePlayer()
+        when (data.languageName) {
+            LanguageName.PORTUGUESE -> showLegalTextPortuguese()
+            LanguageName.DUTCH -> showLegalTextDutch()
+            else -> hideLegalText()
         }
         tvBlackWord.text = data.blackWord
     }
@@ -65,72 +59,19 @@ class BlackWordViewFragment
     }
 
 
-    private fun showYoutubePlayer() {
-//        val fm: FragmentManager? = requireActivity().supportFragmentManager
-//        fm?.beginTransaction()
-//                ?.hide(youtubesupportfragment)
-//                ?.commit()
+    private fun showLegalTextPortuguese() {
+        tvLegalText.text = resources.getString(R.string.portugueseLegalText)
+        tvLegalText.visibility = View.VISIBLE
     }
 
-    private fun hideYoutubePlayer() {
-//        val fm: FragmentManager? = requireActivity().supportFragmentManager
-//        fm?.beginTransaction()
-//                ?.show(youtubesupportfragment)
-//                ?.commit()
+    private fun showLegalTextDutch() {
+        tvLegalText.text = resources.getString(R.string.dutchLegalText)
+        tvLegalText.visibility = View.VISIBLE
     }
 
-    private fun setYoutubeVideo(position: Int) {
-//        val ysF = YouTubePlayerSupportFragment()
-//
-//        val fm: FragmentManager = requireActivity().supportFragmentManager
-//        val ft: FragmentTransaction = fm.beginTransaction()
-//        ft.add(R.id.youtubesupportfragment, ysF)
-//        ft.commit()
-
-        val  youTubePlayerFragment: YouTubePlayerSupportFragment = requireActivity().supportFragmentManager
-                .findFragmentById(R.id.youtubesupportfragment) as YouTubePlayerSupportFragment
-
-
-//        val youTubePlayerFragment =
-//                requireActivity().supportFragmentManager.findFragmentById(R.id.youtubesupportfragment)
-//                        as YouTubePlayerFragment?
-//        youTubePlayerFragment!!.initialize(PlayerConfig.API_KEY, this)
-
-        val mYoutubeLinks = resources.getStringArray(R.array.youtube_links)
-
-
-        val onInitializedListener: YouTubePlayer.OnInitializedListener = object : YouTubePlayer.OnInitializedListener {
-            override fun onInitializationSuccess(provider: YouTubePlayer.Provider, youTubePlayer: YouTubePlayer, b: Boolean) {
-                Log.i("autolog", "mYoutubeLinks[position]: " + mYoutubeLinks[position]);
-                youTubePlayer.loadVideo(mYoutubeLinks[position])
-                youTubePlayer.play()
-            }
-
-            override fun onInitializationFailure(provider: YouTubePlayer.Provider, youTubeInitializationResult: YouTubeInitializationResult) {
-                if (youTubeInitializationResult.isUserRecoverableError) {
-                    youTubeInitializationResult.getErrorDialog(activity, RECOVERY_DIALOG_REQUEST).show()
-                } else {
-                    val errorMessage = String.format("There was an error initializing the YouTubePlayer (%1\$s)", youTubeInitializationResult.toString())
-                    Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-
-        youTubePlayerFragment?.initialize(PlayerConfig.API_KEY, onInitializedListener)
+    private fun hideLegalText() {
+        tvLegalText.visibility = View.GONE
     }
 
-//    override fun onInitializationSuccess(p0: YouTubePlayer.Provider, youTubePlayer: YouTubePlayer, wasRestored: Boolean) {
-//        if (!wasRestored) {
-//            youTubePlayer.cueVideo("nCgQDjiotG0");
-//        }
-//    }
-//
-//    override fun onInitializationFailure(provider: YouTubePlayer.Provider, youTubeInitializationResult: YouTubeInitializationResult) {
-//        if (youTubeInitializationResult.isUserRecoverableError) {
-//            youTubeInitializationResult.getErrorDialog(activity, RECOVERY_DIALOG_REQUEST).show()
-//        } else {
-//            val errorMessage = String.format("There was an error initializing the YouTubePlayer (%1\$s)", youTubeInitializationResult.toString())
-//            Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
-//        }
-//    }
+
 }
